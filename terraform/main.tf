c1b5964d14157958d00df2fc468289b8b661fc28
Muddasar Hassan <mudsr.hsn@gmail.com>
2025-02-16 08:59:49 +0500

    variables added to main.tf, variables.tf ignored

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,20 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
+variable "location" {
+  description = "The Azure region to deploy resources in"
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  default     = "newsverify-rg"
+}
+
+variable "subscription_id" {
+  description = "256c47d7-6c5f-4e92-b116-d234771e3690"
+}
+
 # Resource Group
 resource "azurerm_resource_group" "newsverify" {
   name     = var.resource_group_name
