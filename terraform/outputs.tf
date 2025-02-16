316e2020deb425b73eb8803677519419b7cc623e
Muddasar Hassan <mudsr.hsn@gmail.com>
2025-02-16 10:05:08 +0500

    output.tf modified just for resource group & storage account

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,14 +6,14 @@ output "storage_account_name" {
   value = azurerm_storage_account.state.name
 }
 
-output "cosmosdb_account_name" {
-  value = azurerm_cosmosdb_account.newsverify.name
-}
+# output "cosmosdb_account_name" {
+#   value = azurerm_cosmosdb_account.newsverify.name
+# }
 
-output "function_app_name" {
-  value = azurerm_function_app.newsverify.name
-}
+# output "function_app_name" {
+#   value = azurerm_function_app.newsverify.name
+# }
 
-output "application_insights_name" {
-  value = azurerm_application_insights.newsverify.name
-}
+# output "application_insights_name" {
+#   value = azurerm_application_insights.newsverify.name
+# }
